413cfa4ab0f51c41622dc8584ca9c7e5c56e8db8
Tankmeister93 <frank.friesen19@gmail.com>
2020-02-29 10:20:33 -0700

    Added a new Array, as well as a push method.

--- Part 2/A1/simple-js-app/task 1.2/js/scripts.js
+++ Part 2/A1/simple-js-app/task 1.2/js/scripts.js
@@ -1,20 +1,32 @@
-var repository = ['creatureName', 'creatureHeight', 'creatureTypes', 'creatureAbilities'];
-creatureName = 'Carnivine'
-creatureHeight = 4.07
-creatureAbilities = ['Levitate']
-creatureTypes = ['Grass']
-
-creatureName = 'Metapod'
-creatureHeight = 2.04
-creatureAbilities = ['Shed-skin']
-creatureTypes = ['Bug']
-
-creatureName = 'Charmander'
-creatureHeight = 2.00
-creatureAbilities = ['Blaze', 'Solar-power']
-creatureTypes = ['Fire']
-
-creatureName = 'Squirtle'
-creatureHeight = 0.5
-creatureAbilities = ['Rain-dish', 'Torrent']
-creatureTypes = ['Monster', 'Water  1']
+var myArray = ['Carvine', 'Metapod', 'Charmander', 'Squirtle'];
+var repository = [
+{
+  creatureName : 'Carnivine',
+  creatureHeight : 4.07,
+  creatureAbilities : ['Levitate'],
+  creatureTypes : ['Grass'],
+},
+myArray.push(myArray),
+{
+  creatureName : 'Metapod',
+  creatureHeight : 2.04,
+  creatureAbilities : ['Shed-skin'],
+  creatureTypes : ['Bug'],
+},
+myArray.push(myArray),
+{
+  creatureName : 'Charmander',
+  creatureHeight : 2.00,
+  creatureAbilities : ['Blaze', 'Solar-power'],
+  creatureTypes : ['Fire'],
+},
+myArray.push(myArray),
+{
+  creatureName : 'Squirtle',
+  creatureHeight : 0.5,
+  creatureAbilities : ['Rain-dish', 'Torrent'],
+  creatureTypes : ['Monster', 'Water  1'],
+},
+myArray.push(myArray),
+];
+document.write(myArray)
