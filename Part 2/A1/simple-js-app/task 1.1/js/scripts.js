2e6a4b880c17ca802c2feb4c86e0462734159e5a
Tankmeister93 <frank.friesen19@gmail.com>
2020-03-22 14:47:42 -0600

    started a new javaScript file.

---
+++ Part 2/A1/simple-js-app/task 1.1/js/scripts.js
@@ -0,0 +1,9 @@
+alert('Hello World')
+var favoriteFood = 'pizza';
+document.write(favoriteFood)
+var name = ' Frank';
+document.write(name)
+var age = 25;
+document.write(age)
+var favoritePastTime = 'Art';
+document.write(favoritePastTime)
